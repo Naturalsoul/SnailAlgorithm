b943c5dac73a6d1d01e60f20858224ec570810b5
Naturalsoul <maxr7@live.cl>
2016-06-25 16:27:55 -0400

    Added the Main class and the menu.

---
+++ src/SnailAlgorithm/Main.java
@@ -0,0 +1,20 @@
+package SnailAlgorithm;
+
+/**
+ * The Snail Algorithm is about something like a circle. Here I take a list of disorganized numbers
+ * to organize them and create the snail. You can look here to realize what i'm doing: https://goo.gl/VBMhVN
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static void menu() {
+        System.out.println("----------");
+        System.out.println("Hey!! Tell me, what do u want?");
+        System.out.println("1) Show me how this works.");
+        System.out.println("2) Let me try! I want to enter my own numbers.");
+        System.out.println("9) That was awesome :D!. See ya later!");
+        System.out.println("----------");
+    }
+}
